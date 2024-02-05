7cf59bb844f4ab8cd462445735b3e9faabf0f6af
Hank Goddard <me@henrygoddard.com>
2024-02-04 20:59:44 -0500

    test: skip button unit tests leave examples commented out

--- app/components/Button/Button.test.tsx
+++ app/components/Button/Button.test.tsx
@@ -3,18 +3,18 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Button from ".";
 
-describe("Button", () => {
-  test("should render button with correct text", () => {
-    const { getByText } = render(<Button>Click Me</Button>);
-    const buttonElement = getByText("Click Me");
-    expect(buttonElement).toBeInTheDocument();
-  });
+// describe("Button", () => {
+//   test("should render button with correct text", () => {
+//     const { getByText } = render(<Button>Click Me</Button>);
+//     const buttonElement = getByText("Click Me");
+//     expect(buttonElement).toBeInTheDocument();
+//   });
 
-  test("should call onClick function when button is clicked", () => {
-    const onClick = jest.fn();
-    const { getByText } = render(<Button onClick={onClick}>Click Me</Button>);
-    const buttonElement = getByText("Click Me");
-    fireEvent.click(buttonElement);
-    expect(onClick).toHaveBeenCalled();
-  });
-});
+//   test("should call onClick function when button is clicked", () => {
+//     const onClick = jest.fn();
+//     const { getByText } = render(<Button onClick={onClick}>Click Me</Button>);
+//     const buttonElement = getByText("Click Me");
+//     fireEvent.click(buttonElement);
+//     expect(onClick).toHaveBeenCalled();
+//   });
+// });
