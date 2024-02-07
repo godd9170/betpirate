9f4469a7b9be858841f46e4286379bbdbe3f6b84
Hank Goddard <me@henrygoddard.com>
2024-02-07 01:04:15 -0500

    chore: banner image

--- app/routes/login/components/PhoneNumberForm.tsx
+++ app/routes/login/components/PhoneNumberForm.tsx
@@ -8,13 +8,12 @@ export default function PhoneNumberForm({ error }: { error: any }) {
   const [phone, setPhone] = useState<E164Number | undefined>("");
   return (
     <>
-      <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <Logo height={80} />
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <h1 className="font-primary text-xl text-center font-black">
-            Bet Pirate
-          </h1>
-        </div>
+      <div className="py-10 flex flex-col items-center justify-items-center w-full">
+        <h1 className="text-xl font-black pb-4">Superbowl Prop Picks</h1>
+        <img
+          className="rounded"
+          src="https://media.canva.com/1/image-resize/1/800_670_100_PNG_F/czM6Ly9tZWRpYS1wcml2YXRlLmNhbnZhLmNvbS9tY24tby9NQUY4RHJtY24tby8xL3AucG5n?osig=AAAAAAAAAAAAAAAAAAAAANqSs9FZyxKsuGvgxy5TuyShPXMj1EZAMTp74JaJDtdD&exp=1707299776&x-canva-quality=screen&csig=AAAAAAAAAAAAAAAAAAAAABwzhztgd51sk0D6OhIramE-9yaBAfwBGD8_dkGM-gBQ"
+        />
       </div>
       <div>
         <div>
