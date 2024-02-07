058d45327297ecc01f60b8e79d094af4f31be68d
Hank Goddard <me@henrygoddard.com>
2024-02-07 00:26:36 -0500

    chore: support multi options

--- app/components/PropositionCard/PropositionCardOption.tsx
+++ app/components/PropositionCard/PropositionCardOption.tsx
@@ -1,31 +1,30 @@
+import { ChangeEventHandler } from "react";
+
 export default function PropositionCardOption({
   propositionId,
   option,
-  selected,
-  handleSelect,
+  onChange,
 }: {
   propositionId: string;
   option: any;
-  selected: boolean;
-  handleSelect: Function;
+  onChange: ChangeEventHandler<HTMLInputElement>;
 }) {
   return (
-    <label className={`block w-full text-center`}>
+    <div className="w-full">
       <input
         type="radio"
+        id={option?.id}
         name={propositionId}
         value={option?.id}
-        checked={selected}
-        className="absolute invisible"
-        onChange={() => handleSelect()}
+        className="peer absolute invisible"
+        onChange={onChange}
       />
-      <div
-        className={`grid h-20 flex-grow card bg-base-200 rounded-box place-items-center cursor-pointer  ${
-          selected ? "bg-error" : ""
-        }`}
+      <label
+        className="block w-full grid h-20 flex-grow card rounded-box place-items-center cursor-pointer bg-base-200 text-center peer-checked:bg-error"
+        htmlFor={option?.id}
       >
         {option?.title}
-      </div>
-    </label>
+      </label>
+    </div>
   );
 }
