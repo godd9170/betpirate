00c8d9edeb73ae9d98f1b5282b0eca1aa209cd0f
Hank Goddard <me@henrygoddard.com>
2024-02-10 21:33:20 -0500

    feat: add link to sheet admin for admins

--- app/routes/sheets.$sheetId.tsx
+++ app/routes/sheets.$sheetId.tsx
@@ -1,6 +1,7 @@
 import { json, LoaderFunctionArgs, redirect } from "@remix-run/node";
 import { Link, Outlet, useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
+import { readSailor } from "~/models/sailor.server";
 import { readSheet } from "~/models/sheet.server";
 import { authenticator } from "~/services/auth.server";
 
@@ -9,6 +10,7 @@ export const loader = async ({ params, request }: LoaderFunctionArgs) => {
     failureRedirect: "/login",
   });
   invariant(sailorId, `sailorId is required`);
+  const sailor = await readSailor(sailorId);
 
   const sheetId = params.sheetId;
   invariant(sheetId, `sheetId is required`);
@@ -16,17 +18,43 @@ export const loader = async ({ params, request }: LoaderFunctionArgs) => {
   const sheet = await readSheet(sheetId);
   invariant(!!sheet, "No sheet exists with this id");
 
-  return json({ sheet });
+  return json({ sheet, sailor });
 };
 
 export default function Sheet() {
-  const { sheet } = useLoaderData<typeof loader>();
+  const { sheet, sailor } = useLoaderData<typeof loader>();
   return (
     <div>
-      <header className="mb-2">
+      <header className="mb-2 relative">
         <Link to={`/sheets/${sheet.id}`}>
           <h1 className="text-xl font-black text-center py-4">{sheet.title}</h1>
         </Link>
+        {!!sailor?.admin && (
+          <Link
+            to={`/sheets/${sheet.id}/admin`}
+            className="absolute top-4 right-0"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+          </Link>
+        )}
       </header>
       <Outlet />
     </div>
